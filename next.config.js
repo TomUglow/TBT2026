55ade3491f4f442740adcd621bc57ae30295ced5
TomUglow <tom.uglow@hotmail.com>
2026-02-16 10:58:52 +1100

    Vercel changes

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
-  outputFileTracingExcludes: {
-    '*': [
-      'node_modules/sharp',
-      'node_modules/@swc',
-    ],
-  },
 }
 
 module.exports = nextConfig
